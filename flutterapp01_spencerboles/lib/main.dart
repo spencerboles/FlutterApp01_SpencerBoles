ef92580371252779352d70c66e7513c34c29297b
Spencer Boles <48936680+spencerboles@users.noreply.github.com>
2020-09-24 21:50:54 -0600

    Create columns and add container to column 1 and column 2

--- flutterapp01_spencerboles/lib/main.dart
+++ flutterapp01_spencerboles/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget
   {
     return MaterialApp(
       home: Scaffold(
+         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('Row and Column Widgets'), 
           backgroundColor: Colors.blue, 
@@ -20,18 +22,62 @@ class MyApp extends StatelessWidget
         children: [
           Column(
             children: <Widget>[
-              Container(
-                color: Colors.orange, 
-                width: 100.0, 
-                height: 100.0, 
-                
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Container(
+                    color: Colors.orange, 
+                    width: 100.0, 
+                    height: 100.0, 
+                    
+                    child: Center(child: Text('Container 1')), 
+
+                   
+                    
+                  ),
+                ),
+              ), 
+              Transform.rotate( 
+                angle: pi/4, 
+                child: Column(
+                  children: [
+                    Container(
+                    color: Colors.white, 
+                    height: 100.0, 
+                    width:100.0, 
+                    child: Center(child: Text('Container 2'))
+              ),
+                  ],
+                ) 
               )
 
             ],
           ), 
           Column(
             children: [
+              Expanded( 
+                child: Padding( 
+                  padding: EdgeInsets.all(10.0),
+                  child: Container( 
+                width: 100.0, 
+                color: Colors.yellow, 
 
+
+              ) 
+                )
+              ), 
+
+              Expanded( 
+                child: Padding( 
+                  padding: EdgeInsets.all(10.0),
+                  child: Container( 
+                width: 100.0, 
+                color: Colors.blue, 
+
+
+              ) 
+                )
+              )
 
             ],
           ), 
