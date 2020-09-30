2271b4c204c83bbac4fc0febd870b2011708d762
spencerboles <sboles257@gmail.com>
2020-09-29 21:12:35 -0600

    configure layout

--- flutterapp01_spencerboles/lib/main.dart
+++ flutterapp01_spencerboles/lib/main.dart
@@ -1,94 +1,139 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
   runApp(MyApp());
 }
- 
-class MyApp extends StatelessWidget
-{
-  @override 
-  Widget build(BuildContext context)
-  {
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-         backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text('Row and Column Widgets'), 
-          backgroundColor: Colors.blue, 
-        ), 
-      body: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Center(
-                  child: Container(
-                    color: Colors.orange, 
-                    width: 100.0, 
-                    height: 100.0, 
-                    
-                    child: Center(child: Text('Container 1')), 
-
-                   
-                    
-                  ),
-                ),
-              ), 
-              Transform.rotate( 
-                angle: pi/4, 
+          title: Text('Row and Column Widgets'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                        child: Container(
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            border: Border.all(color: Colors.black, width: 3),
+                          ),
+                          child: Center(child: Text('Container 1')),
+                        ),
+                      ),
+
+                    Transform.rotate(
+                      angle: pi / 4,
+                      child: Column(
+                        children: [
+                          Container(
+                              color: Colors.white,
+                              height: 100.0,
+                              width: 100.0,
+                              child: Center(child: Text('Container 2'))),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                        child: Container(
+                          width: 100.0,
+                          height: 100.0,
+                          color: Colors.yellow,
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text('Container 3'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10.0, 0, 10.0),
+                        child: Container(
+                          width: 100.0, 
+                          color: Colors.blue,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text('Container 4'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.max,
                   children: [
                     Container(
-                    color: Colors.white, 
-                    height: 100.0, 
-                    width:100.0, 
-                    child: Center(child: Text('Container 2'))
-              ),
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Container(
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            shape: BoxShape.circle,
+                            border: Border.all(color: Colors.white, width: 3),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Container 5',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(0, 10.0, 0, 0),
+                        child: Container(
+                            width: 100.0,
+                            height: 100.0,
+                            color: Colors.red,
+                            child:
+                                Text('Con 6', style: TextStyle(fontSize: 30))),
+                      ),
+                    )
                   ],
-                ) 
-              )
-
-            ],
-          ), 
-          Column(
-            children: [
-              Expanded( 
-                child: Padding( 
-                  padding: EdgeInsets.all(10.0),
-                  child: Container( 
-                width: 100.0, 
-                color: Colors.yellow, 
-
-
-              ) 
-                )
-              ), 
-
-              Expanded( 
-                child: Padding( 
-                  padding: EdgeInsets.all(10.0),
-                  child: Container( 
-                width: 100.0, 
-                color: Colors.blue, 
-
-
-              ) 
-                )
-              )
-
-            ],
-          ), 
-          Column(
-            children: [
-
-
+                ),
+              ),
             ],
           ),
-        ],
-      ), 
       ),
     );
   }
